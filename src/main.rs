ebf56a34840da5a5bca30002f5f4bd576b159ac4
Povilas Balciunas <balciunas90@gmail.com>
2017-08-18 09:30:56 +0300

    Move Device struct to tuntap module

--- src/main.rs
+++ src/main.rs
@@ -2,38 +2,12 @@ extern crate nix;
 extern crate libc;
 
 use std::io;
-use std::process::Command;
-
-use nix::fcntl;
-use nix::sys::stat::Mode;
 
 mod tuntap;
 
-const TUNSETIFF : u64 = 0x400454ca;
-
-fn create_vnet_device(tun_file: i32, name: &str) -> Result<i32, i32> {
-    let ifreq = tuntap::InterfaceRequest::with_name(name);
-    unsafe {
-        match libc::ioctl(tun_file, TUNSETIFF, &ifreq) {
-            -1 => Err(-1),
-            _ => Ok(0),
-        }
-    }
-}
-
 fn main() {
-    let fid = fcntl::open("/dev/net/tun", fcntl::O_RDWR, Mode::empty()).unwrap();
-    create_vnet_device(fid, "tun0");
-
-    Command::new("ip")
-        .args(&["addr", "add", "10.0.0.0/24", "dev", "tun0"])
-        .spawn()
-        .expect("Failed to assign IP address.");
-
-    Command::new("ip")
-        .args(&["link", "set", "dev", "tun0", "up"])
-        .spawn()
-        .expect("Failed to set interface up");
+    let tuntap = tuntap::Device::new("tun0".to_string(), "10.0.1.0/24".to_string());
+    tuntap.up();
 
     let mut ln = String::new();
     io::stdin().read_line(&mut ln);
--- src/tuntap.rs
+++ src/tuntap.rs
@@ -1,4 +1,53 @@
 use libc::IF_NAMESIZE;
+use libc;
+
+use std::process::Command;
+
+use nix::fcntl;
+use nix::sys::stat::Mode;
+
+pub struct Device {
+    name: String,
+    addresses: String,
+}
+
+impl Device {
+    pub fn new(name: String, addresses: String) -> Device {
+        Device {
+            name: name,
+            addresses: addresses,
+        }
+    }
+
+    pub fn up(&self) {
+        let fid = fcntl::open("/dev/net/tun", fcntl::O_RDWR, Mode::empty()).unwrap();
+
+        let dev_name = &self.name[..];
+        create_vnet_device(fid, dev_name);
+
+        Command::new("ip")
+            .args(&["addr", "add", "10.0.0.0/24", "dev", dev_name])
+            .spawn()
+            .expect("Failed to assign IP address.");
+
+        Command::new("ip")
+            .args(&["link", "set", "dev", dev_name, "up"])
+            .spawn()
+            .expect("Failed to set interface up");
+    }
+}
+
+const TUNSETIFF : u64 = 0x400454ca;
+
+fn create_vnet_device(tun_file: i32, name: &str) -> Result<i32, i32> {
+    let ifreq = InterfaceRequest::with_name(name);
+    unsafe {
+        match libc::ioctl(tun_file, TUNSETIFF, &ifreq) {
+            -1 => Err(-1),
+            _ => Ok(0),
+        }
+    }
+}
 
 pub enum VirtualDeviceType {
     Tun = 1,
